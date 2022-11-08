dbc2f5a552f7cb906f39cdd6fa80346053d330f7
Michael Chop <007micle007@gmail.com>
2022-11-09 02:43:21 +0300

    Created mock server API, using json-server package

--- pages/posts.js
+++ pages/posts.js
@@ -1,9 +1,23 @@
+import {useState, useEffect} from 'react'
 import {MainLayout} from "../layout/MainLayout";
 
 export default function Posts() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        async function load() {
+            const response = await fetch('http://localhost:9999/posts')
+            const json = await response.json()
+            setPosts(json)
+        }
+
+        load()
+    }, [])
+
     return (
         <MainLayout title={"Posts"}>
             <h1>Posts</h1>
+            <pre>{JSON.stringify(posts, null, 2)}</pre>
         </MainLayout>
     )
 }
