a8f5cad919f64f77987dacc70a4683fb4a7a53aa
Michael Chop <007micle007@gmail.com>
2022-11-09 03:22:26 +0300

    Added the ability to upload posts from the server

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,12 +1,48 @@
 import {useRouter} from "next/router";
 import {MainLayout} from "../../layout/MainLayout";
+import Link from "next/link";
+import {useEffect, useState} from "react";
 
-export default function Post() {
+export default function Post({post: serverPost}) {
+    const [post, setPost] = useState(serverPost)
     const router = useRouter()
+
+    useEffect(() => {
+        async function load() {
+            const response = await fetch(`http://localhost:9999/posts/${router.query.id}`)
+            const data = await response.json()
+            setPost(data)
+        }
+
+        if(!serverPost) {
+            load()
+        }
+    }, [])
+
+    if(!post) {
+        return <MainLayout>
+            <h3>Loading...</h3>
+        </MainLayout>
+    }
 
     return (
         <MainLayout title={"Post"}>
-            <h1>Post {router.query.id}</h1>
+            <h1>{post.title}</h1>
+            <hr/>
+            <p>{post.body}</p>
+            <Link href={"/posts"}>Back to all posts</Link>
         </MainLayout>
     )
+}
+
+Post.getInitialProps = async (context) => {
+    if(!context.req) {
+        return {post: null}
+    }
+    const response = await fetch(`http://localhost:9999/posts/${context.query.id}`)
+    const post = await response.json()
+
+    return {
+        post
+    }
 }
--- pages/posts.js
+++ pages/posts.js
@@ -1,8 +1,9 @@
 import {useState, useEffect} from 'react'
 import {MainLayout} from "../layout/MainLayout";
+import Link from "next/link";
 
-export default function Posts() {
-    const [posts, setPosts] = useState([])
+export default function Posts({posts: serverPosts}) {
+    const [posts, setPosts] = useState(serverPosts)
 
     useEffect(() => {
         async function load() {
@@ -11,13 +12,39 @@ export default function Posts() {
             setPosts(json)
         }
 
-        load()
+        if(!serverPosts) {
+            load()
+        }
     }, [])
+
+    if(!posts) {
+        return <MainLayout>
+            <h3>Loading...</h3>
+        </MainLayout>
+    }
 
     return (
         <MainLayout title={"Posts"}>
             <h1>Posts</h1>
-            <pre>{JSON.stringify(posts, null, 2)}</pre>
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>
+                        <Link href={`/post/[id]`} as={`/post/${post.id}`}>{post.title}</Link>
+                    </li>
+                ))}
+            </ul>
         </MainLayout>
     )
+}
+
+Posts.getInitialProps = async ({req}) => {
+    if(!req) {
+        return {posts: null}
+    }
+    const response = await fetch('http://localhost:9999/posts')
+    const posts = await response.json()
+
+    return {
+        posts
+    }
 }
